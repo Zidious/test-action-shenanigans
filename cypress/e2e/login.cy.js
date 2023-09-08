cdeff2d169dca7fe67666e9d9375d4ea20e7cdd0
Gabe <41127686+Zidious@users.noreply.github.com>
2023-09-08 19:45:21 +0100

    a11y thres 0

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,14 @@
-describe('My Login Application', () => {
-  it('should login with valid credentials', () => {
-    cy.visit('https://the-internet.herokuapp.com/login')
-      .get('#username')
-      .type('tomsmith')
-      .get('#password')
-      .type('SuperSecretPassword!')
+describe("My Login Applicationasd", () => {
+  it("should login with valid credentials", () => {
+    cy.visit("https://the-internet.herokuapp.com/login")
+      .get("#username")
+      .type("tomsmith")
+      .get("#password")
+      .type("SuperSecretPassword!")
       .get('button[type="submit"]')
       .click()
       .wait(1000)
-      .get('#flash')
-      .should('exist')
-  })
-})
+      .get("#flash")
+      .should("exist");
+  });
+});
